70a1be40275b789bdedeb00864a55e2b91a7f718
nvd11 <nvd11@163.com>
2023-10-15 04:51:29 +0800

    #3 fixed proxy issue in gcp vm

--- Dataflow-demo/src/main/java/org/example/Main.java
+++ Dataflow-demo/src/main/java/org/example/Main.java
@@ -26,7 +26,8 @@ public class Main {
         String hostname = localHost.getHostName();
         log.info("current hostname is: ".concat(hostname));
 
-        if (!hostname.contains("gcp")){
+        //skip proxy setup for gcp vm
+        if (!hostname.contains("instance-")){
             System.setProperty("http.proxyHost", "10.0.1.223");
             System.setProperty("http.proxyPort", "7887");
             System.setProperty("https.proxyHost", "10.0.1.223");
